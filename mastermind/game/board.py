7508ace1061a8f04b00f9ec9f288020b76a7ef66
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2021-10-21 23:37:32 -0600

    I updated the files

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -2,21 +2,37 @@ import random
 
 class Board:
 
-     def __init__(self):
-        self.code = 0
+   def __init__(self):
+        self.code = ""
         self._prepare
-        
-        
+        self.guess = 0
+      
 
-     def apply(self, move):
-        guess = move.get_guess()
-    
-     def _prepare(self):
-        self.code = random.randint(1000, 9999)
-        print(self.code)
+   def apply(self, move):
+        self.guess = move.get_code()
+        
+   
 
-code = Board()
-code._prepare()
+   def to_string(self):
+      text = ""
+      string_guess = str(self.guess)
+      for i, digit in enumerate(self.code):
+         if digit == string_guess[i]: text += ("X")
+         elif digit in string_guess[i]: text += ("O")
+         else:
+            text += ("*")
+      return(text)
+
+         
+
+
+
+   def _prepare(self):
+        random_number = random.randint(1000, 9999)
+        self.code = str(random_number)
+
+# code = Board()
+# code._prepare()
         
 
 
