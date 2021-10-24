09d0c7ee60a55796e484b10f3f60aaa2dcffb554
joshudo <joshua.d.ortiz93@gmail.com>
2021-10-23 20:07:54 -0600

    adding Elie's comments

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -3,26 +3,40 @@ import random
 
 
 class Board:
-
-   def __init__(self):
-
-        self.code = ""
-        self._prepare()
-        self.guess = 0
+     """A designated playing surface. The responsibility of Board is to keep track of the pieces in play.
+     Stereotype: 
+        Information Holder
+     Attributes:
+        code(str): to return a random number between 1000 and 9999.
+    """
+     def __init__(self):
+        """The class constructor.
+        Args:
+            self (Board): an instance of Board.
+        """
+          self.code = ""
+          self._prepare()
+          self.guess = 0
       
 
-   def apply(self, move):
+     def apply(self, move):
+        """Applies the given move to the playing surface. In this case, it gets code from the player.
+        Args:
+            self (Board): an instance of Board.
+            move (Move): The move to apply.
+        """
         self.guess = move.get_code()
 
 
-   def _prepare(self):
-        random_number = random.randint(1000, 9999)
-        self.code = str(random_number)
+     def _prepare(self):
+         """Sets up the board with a random number that the player need to guess.
+        Args:
+            self (Board): an instance of Board.
+        """
+          random_number = random.randint(1000, 9999)
+          self.code = str(random_number)
 
 
-# code = Board()
-# code._prepare()
-        
 
 
 
--- mastermind/game/move.py
+++ mastermind/game/move.py
@@ -1,8 +1,20 @@
 class Move:
-
+    """The Move class. The responsibility of it is to keep track of the players.
+    Stereotypy:
+        Information Holder
+    Attributes:
+        _code (int): Code to be guessed.
+    """
     def __init__(self, code):
-
+         """The class constructor.
+        Args:
+            self (Board): an instance of Board.
+        """
         self._code = code
 
     def get_code(self):
+        """Returns the code to be guessed.
+        Args:
+            self (Guess): an instance of Guess.
+        """
         return self._code
